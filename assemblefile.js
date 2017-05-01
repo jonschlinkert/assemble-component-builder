153549d3a7b8c74a49bca0a9d55759050c26e706
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-30 20:48:02 -0400

    use `options.ignore`

--- assemblefile.js
+++ assemblefile.js
@@ -1,39 +1,16 @@
 
 var assemble = require('assemble')();
-var browserSync = require('browser-sync').create();
-var fs = require('fs');
-var rename = require('gulp-rename');
 
-assemble.helpers(require('handlebars-helpers')());
+assemble.task('default', function(done){
+  assemble.partials('src/**/partials/*.hbs');
+  assemble.layouts('src/**/layouts/*.hbs');
+  assemble.data('src/**/data/*.json');
+  assemble.pages('src/**/*.hbs', {
+    ignore: ['**/layouts/**', '**/partials/**']
+  });
 
-assemble.task('load', function(done){
-  assemble.partials([
-    'src/**/partials/*.hbs'
-  ]);
-  assemble.layouts([
-    'src/**/layouts/*.hbs'
-  ]);
-  assemble.data([
-    'src/**/data/*.json'
-  ]);
-  assemble.pages([
-    'src/**/*.hbs',
-    '!src/**/layouts/*.hbs',
-    '!src/**/partials/*.hbs'
-  ]);
+  console.log(assemble.views);
   done();
 });
-
-assemble.task('build', function(){
-  return assemble.toStream('pages')
-                .pipe(assemble.renderFile())
-                .pipe(rename({extname:'.html'}))
-                .pipe(assemble.dest('dist/html'));
-});
-
-assemble.task('default', [
-  'load',
-  'build'
-]);
 
 module.exports = assemble;
