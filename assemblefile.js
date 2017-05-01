c8f25f27b946159206b7a8d05fa9105f2c5570ca
jonschlinkert <jon.schlinkert@sellside.com>
2017-04-30 20:50:29 -0400

    move page to `src/pages`

--- assemblefile.js
+++ assemblefile.js
@@ -4,10 +4,8 @@ var assemble = require('assemble')();
 assemble.task('default', function(done){
   assemble.partials('src/**/partials/*.hbs');
   assemble.layouts('src/**/layouts/*.hbs');
+  assemble.pages(['src/components/**/*.hbs', 'src/**/pages/*.hbs']);
   assemble.data('src/**/data/*.json');
-  assemble.pages('src/**/*.hbs', {
-    ignore: ['**/layouts/**', '**/partials/**']
-  });
 
   console.log(assemble.views);
   done();
